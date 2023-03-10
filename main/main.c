6a94f9967ee81b373000de6855361aff1d11a1a8
lazaroofarrill <lazaroofarrill@gmail.com>
2023-03-10 04:20:30 -0500

    sensor reading

--- main/main.c
+++ main/main.c
@@ -15,10 +15,34 @@
 #include "stdio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "rom/ets_sys.h"
 
 #define IMU_ADDRESS 0x68
 
 const char *IMU_READER_TAG = "IMU_TASK";
+
+void printTask(void *ignore) {
+    esp_err_t err;
+    uint32_t count = 0, lastCount = 0;
+
+    while (1) {
+        count++;
+        err = readLastImuValues();
+        if (err != ESP_OK) {
+            ESP_ERROR_CHECK_WITHOUT_ABORT(err);
+            return;
+        }
+        if (count - lastCount > 1000) {
+            printf("%10d\n", count);
+            fflush(stdout);
+            lastCount = count;
+        }
+//        printf("%10d%10d%10d%10d%10d%10d%10u\n",
+//               IMU.accX, IMU.accY, IMU.accZ,
+//               IMU.gyX, IMU.gyY, IMU.gyZ, count);
+        vTaskDelay(1);
+    }
+}
 
 void app_main(void) {
     esp_err_t err;
@@ -32,19 +56,5 @@ void app_main(void) {
         return;
     }
 
-
-    while (true) {
-        err = readLastImuValues();
-        if (err != ESP_OK) {
-            ESP_ERROR_CHECK_WITHOUT_ABORT(err);
-            return;
-        }
-        printf("%10d%10d%10d%10d%10d%10d\n",
-               IMU.accX, IMU.accY, IMU.accZ,
-               IMU.gyX, IMU.gyY, IMU.gyZ);
-
-        vTaskDelay(1 / portTICK_RATE_MS);
-    }
-
-//    xTaskCreate(mpuReaderTask, IMU_READER_TAG, 2040, NULL, 10, NULL);
+    xTaskCreate(printTask, IMU_READER_TAG, 2040, NULL, 10, NULL);
 }
